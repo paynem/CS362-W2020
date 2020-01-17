2c5a4c0a5c483a30245b969b73d7d622cb50b1fc
paynem <mpayne677@gmail.com>
2020-01-16 17:25:32 -0800

    Added testUtility.py, testDominion1.py, and testDominion2.py for testing purposes.  testUtility.py contains code that initializes the game. testDominion1.py contains code to run the game (and has a money bug). testDominnion2.py contains code to run game (and has victory card bug).

---
+++ projects/paynemi/dominion/testDominion2.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Worked on:Thurs Jan 16 3:48pm 2020
+
+@author: paynemi
+"""
+
+import Dominion
+import testUtility
+import random
+from collections import defaultdict
+
+#Get player names
+player_names = ["Annie", "*Ben", "*Carla"]
+
+#number of curses and victory cards
+nV = testUtility.SetVictoryCards(player_names)
+nC = -10 + 10 * len(player_names)
+nV = 1
+#Define box
+box = testUtility.GetBoxes(nV)
+
+#Define supply_order
+supply_order = testUtility.GetSupplyOrder()
+
+#Define supply
+supply = testUtility.GetSupply(nC, nV, box, player_names)
+
+#initialize the trash
+trash = []
+
+#Construct the Player objects
+players = testUtility.GetPlayers(player_names)
+
+#Play the game
+turn  = 0
+while not Dominion.gameover(supply):
+    turn += 1    
+    print("\r")    
+    for value in supply_order:
+        print (value)
+        for stack in supply_order[value]:
+            if stack in supply:
+                print (stack, len(supply[stack]))
+    print("\r")
+    for player in players:
+        print (player.name,player.calcpoints())
+    print ("\rStart of turn " + str(turn))    
+    for player in players:
+        if not Dominion.gameover(supply):
+            print("\r")
+            player.turn(players,supply,trash)
+            
+
+#Final score
+dcs=Dominion.cardsummaries(players)
+vp=dcs.loc['VICTORY POINTS']
+vpmax=vp.max()
+winners=[]
+for i in vp.index:
+    if vp.loc[i]==vpmax:
+        winners.append(i)
+if len(winners)>1:
+    winstring= ' and '.join(winners) + ' win!'
+else:
+    winstring = ' '.join([winners[0],'wins!'])
+
+print("\nGAME OVER!!!\n"+winstring+"\n")
+print(dcs)
